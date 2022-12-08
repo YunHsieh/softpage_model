88036651f8560c43540836d6c1eca4f3cb9e92c1
yun <kilicapeto@gmail.com>
2022-12-09 01:11:13 +0800

    add: register cli

--- softpage/settings.py
+++ softpage/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+import os
 
 from pathlib import Path
 import dj_database_url
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--l=8v(=*h4_st5d)xtm%(u#gj28gcazn64&yb^h+tz_kp_k*o4'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure--l=8v(=*h4_st5d)xtm%(u#gj28gcazn64&yb^h+tz_kp_k*o4')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
