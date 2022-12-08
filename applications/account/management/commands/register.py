7a63db2e4e0ce35be41b3e155366b58f4d465496
Yun <52973591+YunHsieh@users.noreply.github.com>
2022-12-09 01:11:48 +0800

    Feat/create cli for register fake user (#10)
    * add: register cli
    
    * add: passlib library

---
+++ applications/account/management/commands/register.py
@@ -0,0 +1,25 @@
+from django.core.management.base import BaseCommand, CommandError
+from applications.account.models import CustomUser
+from django.conf import settings
+from passlib.hash import pbkdf2_sha256
+
+
+def make_password(password):
+    return pbkdf2_sha256.hash(f'{password}{settings.SECRET_KEY}')
+
+
+class Command(BaseCommand):
+    help = 'Create the new fake account'
+
+    def add_arguments(self, parser):
+        parser.add_argument('username', nargs='+', type=str)
+        parser.add_argument('password', nargs='+', type=str)
+
+    def handle(self, *args, **options):
+        username, password = options['username'], options['password']
+        instance = CustomUser(
+            username=username,
+            email=f'{username}@example.com',
+            password=make_password(password),
+        )
+        instance.save()
