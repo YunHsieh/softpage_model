95d812b2dd874acc31cd7f25b14f729835f5d97f
yun <kilicapeto@gmail.com>
2022-12-09 01:06:48 +0800

    add: jwt model

--- applications/account/models.py
+++ applications/account/models.py
@@ -9,7 +9,51 @@ class CustomUser(AbstractUser):
     REQUIRED_FIELDS = ['username']
 
     class Meta(AbstractUser.Meta):
-        db_table = "auth"
+        db_table = 'auth'
+
+
+class OutstandingToken(models.Model):
+    id = models.BigAutoField(
+        primary_key=True, 
+        serialize=False,
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, 
+        on_delete=models.SET_NULL, 
+        null=True, 
+        blank=True,
+    )
+
+    jti = models.CharField(
+        unique=True, 
+        max_length=255,
+    )
+    token = models.TextField()
+
+    created_at = models.DateTimeField(null=True, blank=True)
+    expires_at = models.DateTimeField()
+
+    class Meta:
+        db_table = 'account_jwt_token'
+        ordering = ('user',)
+
+    def __str__(self):
+        return "Token for {} ({})".format(
+            self.user,
+            self.jti,
+        )
+
+
+class BlacklistedToken(models.Model):
+    id = models.BigAutoField(primary_key=True, serialize=False)
+    token = models.OneToOneField(OutstandingToken, on_delete=models.CASCADE)
+    blacklisted_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'account_black_list_token'
+
+    def __str__(self):
+        return f"Blacklisted token for {self.token.user}"
 
 
 # class Authemail_verification(models.Model):
